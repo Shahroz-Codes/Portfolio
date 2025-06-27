7849c98ddc789babb3f9c4b95d08955a0cf5ebd6
Shahroz-Codes <shafqatshahroz@gmail.com>
2025-06-27 09:11:05 +0500

    fix: improve layout of subDescription and tags in ProjectDetails component

--- src/components/Projects/ProjectsComponents/ProjectDetails.jsx
+++ src/components/Projects/ProjectsComponents/ProjectDetails.jsx
@@ -28,8 +28,8 @@ const ProjectDetails = ({
           {subDescription.map((subDesc, index) => (
             <p className="mb-3 font-normal text-neutral-400">{subDesc}</p>
           ))}
-          <div className="flex items-center justify-between mt-4">
-            <div className="flex gap-3">
+          <div className="flex flex-wrap space-y-3 items-center justify-between mt-4">
+            <div className="flex flex-wrap gap-3">
               {tags.map((tag) => (
                 <img
                   key={tag.id}
@@ -43,7 +43,6 @@ const ProjectDetails = ({
               href={href}
               target="_blank"
               rel="noopener noreferrer"
-
               className="inline-flex items-center gap-1 font-medium cursor-pointer hover-animation">
               View Project{""}
               <img src="assets/arrow-up.svg" className="size-4" />
