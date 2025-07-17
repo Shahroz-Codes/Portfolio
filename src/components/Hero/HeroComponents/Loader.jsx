5e199977908a48e1684ab3e961454d2424dfb293
Shahroz-Codes <shafqatshahroz@gmail.com>
2025-07-17 16:53:23 +0500

    fix: enhance Loader component layout and styling for better visibility

--- src/components/Hero/HeroComponents/Loader.jsx
+++ src/components/Hero/HeroComponents/Loader.jsx
@@ -3,8 +3,10 @@ import { Html, useProgress } from "@react-three/drei";
 const Loader = () => {
   const { progress } = useProgress();
   return (
-    <Html center className="text-xl font-normal text-center">
-      {progress}% Loaded
+    <Html center className="">
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-purple-500 border-solid">{progress}% Loaded</div>
+      </div>
     </Html>
   );
 };
