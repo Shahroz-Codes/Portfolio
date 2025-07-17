8a588ca9fdd8252717440a9d4208f035c77ebdcb
Shahroz-Codes <shafqatshahroz@gmail.com>
2025-07-17 17:32:33 +0500

    update CV and loader

--- src/components/Hero/HeroComponents/Loader.jsx
+++ src/components/Hero/HeroComponents/Loader.jsx
@@ -2,10 +2,19 @@ import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
   const { progress } = useProgress();
+
   return (
-    <Html center className="">
+    <Html center>
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900">
-        <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-purple-500 border-solid">{progress}% Loaded</div>
+        <div className="relative h-32 w-32">
+          {/* Spinner Circle */}
+          <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-purple-500 border-solid" />
+          
+          {/* Centered Static Text */}
+          <div className="absolute inset-0 flex items-center justify-center text-white text-2xl font-semibold">
+            {Math.floor(progress)}%
+          </div>
+        </div>
       </div>
     </Html>
   );
